8325fde31eb5940ce04da170c9b930aa81f6ccb4
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-05-03 16:20:30 +0700

    feat: add mongoose integration and product model

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("DB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
