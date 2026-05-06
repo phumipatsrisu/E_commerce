3639586215948c2236db8a8208037fc84b652d66
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-05-06 16:30:17 +0700

    feat: enhance product routes with CRUD operations and improve error handling

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,52 @@ const Product = require("../models/Product");
 router.post("/", async (req, res) => {
   try {
     const product = await new Product(req.body).save();
-    res.status(200).json({message:"เซฟข้อมูลสำเร็จ", product:product})
+    res.status(200).json({ message: "เซฟข้อมูลสำเร็จ", product: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ message: "ข้อมูลทั้งหมด", products });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    res.status(200).json({ message: "เจอข้อมูลแล้ว", product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "Updated", product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+    res.status(200).json({ message: "Deleted", product });
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+
 app.use("/api/product", productRouter);
 
 connectDB();
-
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
-app.get("/api", (req, res) => {
-  res.send("hello from simple server :)");
-});
