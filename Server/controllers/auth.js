2d8783d78aeff2e2753a710646d09549f32ade3d
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-05-12 16:00:35 +0700

    feat: add JWT authentication for user login and update auth routes

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -14,8 +15,35 @@ exports.register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     await new User({ username, password: hashedPassword }).save();
-
     res.status(201).json({ message: "สมัครสมาชิกสำเร็จเรียบร้อย!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Password is wrong" });
+    }
+
+    const payload = {
+      user: {
+        id: user._id,
+        role: user.role,
+      },
+    };
+
+    const token = jwt.sign(payload, "mySecretKey123", { expiresIn: "1d" });
+    res.json({ token, payload });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
