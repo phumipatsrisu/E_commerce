18e60c57138da8325a59270b4237739c54a70eca
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-05-17 16:52:50 +0700

    feat: implement registration form in Register component

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,8 +1,22 @@
-
 const Register = () => {
   return (
     <div>
       <h1>Register</h1>
+      <form>
+        <div>
+          <label>
+            <input type="text" placeholder="username..." />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input type="password" placeholder="password..." />
+          </label>
+        </div>
+
+        <button>Register</button>
+      </form>
     </div>
   );
 };
