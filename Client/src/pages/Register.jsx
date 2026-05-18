54ddd5c56f56b25b277ce76ae080caadb39e77d5
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-05-18 15:19:14 +0700

    feat: enhance registration form with state management and submit handling

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>
-            <input type="text" placeholder="username..." />
+            <input
+              type="text"
+              placeholder="username..."
+              name="username"
+              value={form.username}
+              onChange={(e) => handleChange(e)}
+            />
           </label>
         </div>
 
         <div>
           <label>
-            <input type="password" placeholder="password..." />
+            <input
+              type="password"
+              placeholder="password..."
+              name="password"
+              value={form.password}
+              onChange={(e) => handleChange(e)}
+            />
           </label>
         </div>
 
